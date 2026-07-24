61ed68339715b045e8e5d4a1e1261789ffd8d073
denysenko-dev <dmytro.denysenko.dev@gmail.com>
2026-07-24 21:19:58 +0300

    feat: complete sprint 5 tasks (input, select, textarea, checkbox)

--- part1/sprint_05/sprint_05.js
+++ part1/sprint_05/sprint_05.js
@@ -1,8 +1,11 @@
 // Task 1
 // Создана функция t01. Функция запускается при нажатии .b-1. Напишите код внутри функции, который читает строку из .i-1 и выводит в .out-1.
 
-function t01 () {
+function t01() {
+    const i1 = document.querySelector('.i-1').value;
+    const out1 = document.querySelector('.out-1');
 
+    out1.textContent = i1;
 }
 
 document.querySelector('.b-1').onclick = t01;
@@ -11,8 +14,10 @@ document.querySelector('.b-1').onclick = t01;
 // Task 2
 // Создана функция t02. Функция запускается при нажатии .b-2. Напишите код внутри функции, которая присваивает input.i-2 значение value равное 'one piece'.
 
-function t02 () {
+function t02() {
+    const i2 = document.querySelector('.i-2');
 
+    i2.value = 'one piece';
 }
 
 document.querySelector('.b-2').onclick = t02;
@@ -20,7 +25,7 @@ document.querySelector('.b-2').onclick = t02;
 // Task 3
 // Создана функция. Изучите ее работу. Посмотрите, на тип input, и на тип данных, которые выводятся в консоль. Почему происходит так? Если не понятно - задавайте вопрос в чате.
 
-function t03 () {
+function t03() {
     const value = document.querySelector('.i-3').value;
     console.log(value);
     console.log(typeof value);
@@ -32,8 +37,12 @@ document.querySelector('.b-3').onclick = t03;
 // Task 4
 // Создана функция t04. Функция запускается при нажатии .b-4. Напишите код внутри функции, который считывает числа из input.i-41 и input.i-42 и выводит в .out-4 их сумму.
 
-function t04 () {
-    
+function t04() {
+    const i41 = Number(document.querySelector('.i-41').value);
+    const i42 = Number(document.querySelector('.i-42').value);
+    const out4 = document.querySelector('.out-4');
+
+    out4.textContent = i41 + i42;
 }
 
 document.querySelector('.b-4').onclick = t04;
@@ -42,8 +51,11 @@ document.querySelector('.b-4').onclick = t04;
 // Task 5
 // Создана функция t05. Функция запускается при нажатии .b-5. Напишите код внутри функции, который читает строку из input.i-51 и записывает в input.i-5.
 
-function t05 () {
-    
+function t05() {
+    const i51 = document.querySelector('.i-51');
+    const i5 = document.querySelector('.i-5');
+
+    i5.value = i51.value;
 }
 
 document.querySelector('.b-5').onclick = t05;
@@ -51,8 +63,11 @@ document.querySelector('.b-5').onclick = t05;
 // Task 6
 // Создана функция t06. Функция запускается при нажатии .b-6. Напишите код внутри функции, который читает строку из input.i-6 и выводит в .out-6.
 
-function t06 () {
-    
+function t06() {
+    const i6 = document.querySelector('.i-6').value;
+    const out6 = document.querySelector('.out-6');
+
+    out6.textContent = i6;
 }
 
 document.querySelector('.b-6').onclick = t06;
@@ -61,8 +76,11 @@ document.querySelector('.b-6').onclick = t06;
 // Task 7
 // Давайте чуть разнообразим наши знания событий и изучим событие input. Событие срабатывает при вводе информации в input. Напишите в функцию t07 код, который получает value из input.i-7 и выводит в .out-7. Для проверки - начните вводить любой текст в input.i-7.
 
-function t07 () {
-    
+function t07() {
+    const i7 = document.querySelector('.i-7');
+    const out7 = document.querySelector('.out-7');
+
+    out7.textContent = i7.value;
 }
 
 document.querySelector('.i-7').oninput = t07;
@@ -71,8 +89,13 @@ document.querySelector('.i-7').oninput = t07;
 // Task 8
 // Создана функция t08. Функция запускается при нажатии .b-8. Напишите код внутри функции, который читает число из input.i-8 и записывает в progress.p-8. Также, число выводится в .out-8.
 
-function t08 () {
-    
+function t08() {
+    const i8 = document.querySelector('.i-8');
+    const p8 = document.querySelector('.p-8');
+    const out8 = document.querySelector('.out-8');
+
+    p8.value = i8.value;
+    out8.textContent = i8.value;
 }
 
 document.querySelector('.b-8').onclick = t08;
@@ -80,8 +103,11 @@ document.querySelector('.b-8').onclick = t08;
 // Task 9
 // Создана функция t09. Функция запускается при нажатии .b-9. Напишите код внутри функции, который читает цвет из input.i-9 и выводит в .out-9.
 
-function t09 () {
-    
+function t09() {
+    const i9 = document.querySelector('.i-9');
+    const out9 = document.querySelector('.out-9');
+
+    out9.textContent = i9.value;
 }
 
 document.querySelector('.b-9').onclick = t09;
@@ -90,8 +116,12 @@ document.querySelector('.b-9').onclick = t09;
 // Task 10
 // Создана функция t10. Функция запускается при нажатии .b-10. Напишите код внутри функции, который читает цвет из input.i-10 и выводит в .out-10 и окрашивает .out-10 с помощью свойства style.backgroundColor.
 
-function t10 () {
-       
+function t10() {
+    const i10 = document.querySelector('.i-10').value;
+    const out10 = document.querySelector('.out-10');
+
+    out10.textContent = i10;
+    out10.style.backgroundColor = i10;
 }
 
 document.querySelector('.b-10').onclick = t10;
@@ -100,8 +130,11 @@ document.querySelector('.b-10').onclick = t10;
 // Task 11
 // Создана функция t11. Функция запускается при нажатии .b-11. Напишите код внутри функции, который читает строку из input.i-11 и присваивает input.i-111.
 
-function t11 () {
-    
+function t11() {
+    const i11 = document.querySelector('.i-11');
+    const i111 = document.querySelector('.i-111');
+
+    i111.value = i11.value;
 }
 
 document.querySelector('.b-11').onclick = t11;
@@ -109,9 +142,8 @@ document.querySelector('.b-11').onclick = t11;
 // Task 12
 // Создана функция t12. Функция запускается при нажатии .b-12. Напишите код внутри функции, который получает выбранный value из select.s-12 и выводит в .out-12. Напоминаю - не усложняйте! Задача решается одной строкой.
 
-function t12 () {
-   
-    
+function t12() {
+    document.querySelector('.out-12').textContent = document.querySelector('.s-12').value;
 }
 
 document.querySelector('.b-12').onclick = t12;
@@ -120,9 +152,8 @@ document.querySelector('.b-12').onclick = t12;
 // Task 13
 // Создана функция t13. Функция запускается при нажатии .b-13. Напишите код внутри функции, который получает текст между тегами option выбранного option. Напоминаю - не усложняйте! 
 
-function t13 () {
-   
-    
+function t13() {
+    document.querySelector('.out-13').textContent = document.querySelector('.s-13').selectedOptions[0].text;
 }
 
 document.querySelector('.b-13').onclick = t13;
@@ -133,8 +164,8 @@ document.querySelector('.b-13').onclick = t13;
 
 let g14 = 333;
 
-function t14 () {
-   
+function t14() {
+    document.querySelector('.s-14').value = g14;
 }
 
 document.querySelector('.b-14').onclick = t14;
@@ -143,7 +174,7 @@ document.querySelector('.b-14').onclick = t14;
 // Мы получали значение из select по нажатию на кнопку. Давайте изучим еще одно событие, которое очень подходит для select - событие change. Событие срабатывает когда select изменяется. Написана функция t15, функция срабатывает при изменении select.s-15. Напишите код внутри функции, который получает значение value выбранного option и выводит в .out-15.
 
 function t15() {
-
+    document.querySelector('.out-15').textContent = document.querySelector('.s-15').value;
 }
 
 document.querySelector('.s-15').onchange = t15;
@@ -152,8 +183,8 @@ document.querySelector('.s-15').onchange = t15;
 // Task 16
 // Создана функция t16. Функция запускается при нажатии .b-16. Внутри функции получите value из select.s-161 и присвойте value в select.s-162.
 
-function t16 () {
-   
+function t16() {
+    document.querySelector('.s-162').value = document.querySelector('.s-161').value;
 }
 
 document.querySelector('.b-16').onclick = t16;
@@ -161,8 +192,8 @@ document.querySelector('.b-16').onclick = t16;
 // Task 17
 // Создана функция t17. Функция запускается при нажатии .b-17. Функция читает текст из textarea.ta-17 и выводит в .out-17.
 
-function t17 () {
-   
+function t17() {
+    document.querySelector('.out-17').textContent = document.querySelector('.ta-17').value;
 }
 
 document.querySelector('.b-17').onclick = t17;
@@ -171,8 +202,8 @@ document.querySelector('.b-17').onclick = t17;
 // Task 18
 // Создана функция t18. Функция запускается при событии input на input range. Напишите код внутри функции, который получает value из input.i-18 и выводит в .out-18.
 
-function t18 () {
-   
+function t18() {
+    document.querySelector('.out-18').textContent = document.querySelector('.i-18').value;
 }
 
 document.querySelector('.i-18').oninput = t18;
@@ -181,7 +212,7 @@ document.querySelector('.i-18').oninput = t18;
 // Создана функция t19. Функция запускается при клике на кнопку .b-19. Изучите как работает функция. Снимите комментарий с переменной g19 и изучите как меняется checkbox.ch-19.
 
 let g19 = true;
-// g19 = false;
+g19 = false;
 
 const ch19 = document.querySelector('.ch-19');
 
@@ -198,8 +229,12 @@ document.querySelector('.b-19').onclick = t19;
 // Task 20
 // Создана функция t20. Функция запускается при клике на кнопку .b-20. Напишите код внутри функции, который проверяет является ли .ch-20 выбранным (checked) и если да, то выводит в .out-20 value элемента .ch-20.
 
-function t20() {
+const ch20 = document.querySelector('.ch-20');
 
+function t20() {
+    if (ch20.checked) {
+        document.querySelector('.out-20').textContent = ch20.value;
+    }
 }
 
 document.querySelector('.b-20').onclick = t20;
