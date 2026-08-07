90eeffbc0e7353586da199c8f25bfc818633fa80
denysenko-dev <dmytro.denysenko.dev@gmail.com>
2026-08-07 14:18:48 +0300

    chore: initialize base structure for sprint 11

---
+++ part1/sprint_11/sprint_11.js
@@ -0,0 +1,96 @@
+// Task 1
+// Напишите функцию t01, которая добавляет .out-1 класс CSS (далее просто класс) 'bg-orange'.
+
+function t01() {
+
+}
+
+document.querySelector('.b-1').onclick = t01;
+
+// Task 2
+// Напишите функцию t02, которая удаляет у .out-2 класс CSS (далее просто класс) 'bg-orange'.
+
+function t02() {
+
+}
+
+document.querySelector('.b-2').onclick = t02;
+
+// Task 3
+// Напишите функцию t03, которая проверяет наличие класса 'bg-orange' у .block-3. Результат проверки - выводит в .out-3.
+
+function t03() {
+
+}
+
+document.querySelector('.b-3').onclick = t03;
+
+
+// Task 4
+// Напишите функцию t04, которая переключает (toggle) класс 'bg-orange' у элемента .out-4.
+
+function t04() {
+
+}
+
+document.querySelector('.b-4').onclick = t04;
+
+
+// Task 5
+// Напишите функцию t05, которая заменяет класс 'bg-orange' на класс 'bg-bisque'.
+
+function t05() {
+
+}
+
+document.querySelector('.b-5').onclick = t05;
+
+
+// Task 6
+// Напишите функцию t06, которая делает toggle класса 'round-img' для изображения .img-6.
+
+function t06() {
+
+}
+
+document.querySelector('.b-6').onclick = t06;
+
+
+// Task 7
+// Напишите функцию t07, которая добавляет класс 'bold' в .out-7.
+
+function t07() {
+
+}
+
+document.querySelector('.b-7').onclick = t07;
+
+
+// Task 8
+// Напишите функцию t08, которая переключает (toggle) класс 'hide' для блока .out-8.
+
+function t08() {
+
+}
+
+document.querySelector('.b-8').onclick = t08;
+
+
+// Task 9
+// Напишите функцию t09, которая при нажатии кнопки проверяет input.ch-91 и если он выбран, то добавляет класс 'bold' для .out-9. Если input.ch-92 выбран - то добавляет класс 'uppercase' для .out-9. Если checkbox не выбраны, то удаляет данные классы у .out-9.
+
+function t09() {
+
+}
+
+document.querySelector('.b-9').onclick = t09;
+
+
+// Task 10
+// Напишите функцию t10, которая при нажатии кнопки делает toggle класса 'clip' для .out-10.
+
+function t10() {
+
+}
+
+document.querySelector('.b-10').onclick = t10;
