32fb63ad5a0bd7fbe3113d5aed9363f284d26c65
denysenko-dev <dmytro.denysenko.dev@gmail.com>
2026-08-16 20:27:22 +0300

    feat: implement sprint 3 tasks (array search algorithms: filter, reduce, find, findIndex, findLast, forEach)

--- part2/sprint_03/sprint_03.js
+++ part2/sprint_03/sprint_03.js
@@ -3,33 +3,34 @@
 // Task 01
 // Функция должна принимать два аргумента - массив и значение и возращать количество таких значений в массиве.
 
-const t01 = (arr, val) => {
-
-};
+const t01 = (arr, val) => arr.filter((el) => el === val).length;
 
 document.querySelector('.b-1').addEventListener('click', () => {
     const arr_01 = [22, 33, 44, 55, 66, 33, 22, 33, 44, 22, 44, 33];
     document.querySelector('.out-1').textContent = t01(arr_01, 33);
 });
 
+
 // Task 02
 // Функция должна принимать два аргумента - массив и значение и возвращать массив индексов вхождения значений в массив.
 
 const t02 = (arr, val) => {
-
+    return arr.reduce((acc, el, index) => {
+        if (el === val) acc.push(index);
+        return acc;
+    }, []);
 };
 
 document.querySelector('.b-2').addEventListener('click', () => {
-
+    const arr_02 = [22, 33, 44, 55, 66, 33, 22, 33, 44, 22, 44, 33];
+    document.querySelector('.out-2').textContent = t02(arr_02, 33);
 });
 
 
 // Task 03
 // Функция должна принимать три аргумента - массив и значение1 и значение2. Функция должна возвращать массив где все вхождения значения1 заменены на значение2.
 
-const t03 = (arr, val1, val2) => {
-
-};
+const t03 = (arr, val1, val2) => arr.map(el => (el === val1) ? val2 : el);
 
 document.querySelector('.b-3').addEventListener('click', () => {
     const arr_03 = [22, 33, 44, 55, 66, 33, 22, 33, 44, 22, 44, 33];
@@ -41,7 +42,9 @@ document.querySelector('.b-3').addEventListener('click', () => {
 // Функция должна принимать два аргумента - массив и значение. Функция должна заменить первое вхождение значения в массив на нуль и возвратить массив.
 
 const t04 = (arr, val1) => {
-
+    const index = arr.findIndex((el) => el === val1);
+    arr[index] = 0;
+    return arr;
 };
 
 document.querySelector('.b-4').addEventListener('click', () => {
@@ -54,7 +57,9 @@ document.querySelector('.b-4').addEventListener('click', () => {
 // Функция должна принимать два аргумента - массив и значение. Функция должна заменить последнее вхождение значения в массив на нуль и возвратить массив.
 
 const t05 = (arr, val1) => {
-
+    const index = arr.findLastIndex((el) => el === val1);
+    arr[index] = 0;
+    return arr;
 };
 
 document.querySelector('.b-5').addEventListener('click', () => {
@@ -67,7 +72,7 @@ document.querySelector('.b-5').addEventListener('click', () => {
 // Функция должна принимать два массива и проверять, что входящие в них элементы равны. Под равенством понимается сравнение по типу (строгое), по значению, и по положению в массиве. Функция должна возвращать true/false.
 
 const t06 = (arr1, arr2) => {
-
+    return arr1.length === arr2.length && arr1.every((el, index) => el === arr2[index]);
 };
 
 document.querySelector('.b-6').addEventListener('click', () => {
@@ -78,9 +83,7 @@ document.querySelector('.b-6').addEventListener('click', () => {
 // Task 07
 // Функция должна принимать массив и с помощью метода find искать в нем первое значение которое больше 10 но меньше 13 и возвращать данное значение.
 
-const t07 = (arr1) => {
-
-};
+const t07 = (arr1) => arr1.find(el => el > 10 && el < 13);
 
 document.querySelector('.b-7').addEventListener('click', () => {
     const arr_07 = [1, 2, 3, 4, 4, 5, 66, 22, 12, 13, 14];
@@ -91,9 +94,7 @@ document.querySelector('.b-7').addEventListener('click', () => {
 // Task 08
 // Функция должна принимать массив и с помощью метода find искать в нем последнее значение которое больше 10 но меньше 13 и возвращать данное значение.
 
-const t08 = (arr1) => {
-
-};
+const t08 = (arr1) => arr1.toReversed().find(el => el > 10 && el < 13);
 
 document.querySelector('.b-8').addEventListener('click', () => {
     const arr_08 = [1, 12, 3, 4, 4, 5, 66, 22, 12, 11, 14];
@@ -104,22 +105,18 @@ document.querySelector('.b-8').addEventListener('click', () => {
 // Task 09
 // Функция должна принимать массив и значение. Функция должна искать вхождение (не строгое) значения в массив, и возвращать первый индекс вхождения.
 
-const t09 = (arr, val) => {
-
-};
+const t09 = (arr, val) => arr.findIndex((el) => el == val);
 
 document.querySelector('.b-9').addEventListener('click', () => {
     const arr_09 = [1, 12, 3, 4, 4, 5];
-    document.querySelector('.out-9').textContent = t09(arr_09, 4);
+    document.querySelector('.out-9').textContent = t09(arr_09, 12);
 });
 
 
 // Task 10
 // Функция должна принимать массив и значение. Функция должна искать вхождение (не строгое) значения в массив, и возвращать последний индекс вхождения.
 
-const t10 = (arr, val) => {
-
-};
+const t10 = (arr, val) => arr.findLastIndex((el) => el == val);
 
 document.querySelector('.b-10').addEventListener('click', () => {
     const arr_10 = [1, 12, 3, 4, 4, 5];
@@ -134,7 +131,8 @@ let arr_11 = [10, 20, 30, 40];
 let res_11 = [];
 
 const t11 = () => {
-
+    res_11 = [];
+    arr_11.forEach((el, index) => res_11.push(index));
 };
 
 document.querySelector('.b-11').addEventListener('click', () => {
@@ -150,7 +148,10 @@ let arr_12 = [10, 20, 30, 40];
 let res_12 = [];
 
 const t12 = () => {
-
+    res_12 = [];
+    arr_12.forEach((el, index) => {
+        if (el % 2 === 0) res_12.push(index);
+    });
 };
 
 document.querySelector('.b-12').addEventListener('click', () => {
@@ -166,7 +167,8 @@ let arr_13 = [10, 20, 30, 40];
 let res_13 = 0;
 
 const t13 = () => {
-
+    res_13 = 0;
+    arr_13.forEach((el) => res_13 += el);
 };
 
 document.querySelector('.b-13').addEventListener('click', () => {
@@ -183,15 +185,16 @@ let res_14 = [];
 
 const t14 = () => {
     res_14 = [];
-    for (let i = 0; i < arr_14.length; i += 2) {
-        res_14.push(arr_14.at(i));
-    }
+    arr_14.forEach((el, index) => {
+        if (index % 2 === 0) res_14.push(el);
+    });
 };
 
 document.querySelector('.b-14').addEventListener('click', () => {
     t14();
     document.querySelector('.out-14').textContent = res_14;
 });
+
 
 // Task 15
 // Изучите работу функции. Сделайте вывод о методе forEach.
