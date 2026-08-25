f57c25b8ed1602e7261022087b64c1f1c58ee876
denysenko-dev <dmytro.denysenko.dev@gmail.com>
2026-08-25 20:47:02 +0300

    feat: implement sprint 6 tasks 01-10 (array methods: fill, forEach, every, includes, reduce)

--- part2/sprint_06/sprint_06.js
+++ part2/sprint_06/sprint_06.js
@@ -7,8 +7,7 @@
 // Task 01
 // Создайте функцию, которая получает аргумент число n и с помощью Array создает пустой массив длины n. Функция должна возвращать созданный массив.
 
-const t01 = (n) => {
-}
+const t01 = (n) => Array(Number(n));
 
 document.querySelector('.b-1').addEventListener('click', () => console.log(t01(5)));
 
@@ -19,7 +18,7 @@ document.querySelector('.b-1').addEventListener('click', () => console.log(t01(5
 const t02 = () => {
     const arr = [22, 33, 44, 55, 66, 77, 88];
     return arr.fill(0, 2, 4);
-}
+};
 
 document.querySelector('.b-2').addEventListener('click', () => console.log(t02()));
 
@@ -30,8 +29,7 @@ document.querySelector('.b-2').addEventListener('click', () => console.log(t02()
 let arr_03 = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
 console.log(arr_03);
 
-const t03 = (n) => {
-}
+const t03 = (n) => arr_03.fill(Number(n), 3, 8);
 
 document.querySelector('.b-3').addEventListener('click', () => console.log(t03(999)));
 
@@ -39,8 +37,14 @@ document.querySelector('.b-3').addEventListener('click', () => console.log(t03(9
 // Task 04
 //  Напишите функцию, которая при каждом запуске выводит по очереди в .out-4 символ 1 или 2. Т.е. нажали один раз - вывели 1. Нажали второй раз. Вместо единицы - двойка, затем опять 1 и т.д.
 
+const out4 = document.querySelector('.out-4');
+let arr_04 = [1, 2];
+let idx = 0;
+
 const t04 = () => {
-}
+    out4.textContent = arr_04[idx];
+    idx = 1 - idx;
+};
 
 document.querySelector('.b-4').addEventListener('click', t04);
 
@@ -50,8 +54,7 @@ document.querySelector('.b-4').addEventListener('click', t04);
 
 let arr_05 = [100, 200, 300];
 
-const t05 = (arr) => {
-}
+const t05 = (arr) => arr.map((item, index) => index);
 
 document.querySelector('.b-5').addEventListener('click', () => console.log(t05(arr_05)));
 
@@ -62,7 +65,11 @@ document.querySelector('.b-5').addEventListener('click', () => console.log(t05(a
 let arr_06 = [0, 10, 10, 0, 0, 1, 10];
 
 const t06 = (arr) => {
-}
+    return arr.reduce((acc, item, index) => {
+        if (item === 10) acc.push(index);
+        return acc;
+    }, []);
+};
 
 document.querySelector('.b-6').addEventListener('click', () => console.log(t06(arr_06)));
 
@@ -70,8 +77,7 @@ document.querySelector('.b-6').addEventListener('click', () => console.log(t06(a
 // Task 07
 //  Напишите функцию, которая сравнивает два массива и если у них одинаковая длина, одинаковый порядок элементов и значения одинаковы (строгое сравнение) возвращает true. Если нет - false.
 
-const t07 = (a, b) => {
-}
+const t07 = (a, b) => a.length === b.length && a.every((item, index) => item === b[index]);
 
 document.querySelector('.b-7').addEventListener('click', () => console.log(t07([1, 3], [1, 3])));
 
@@ -80,8 +86,7 @@ document.querySelector('.b-7').addEventListener('click', () => console.log(t07([
 //  Напишите функцию, которая сравнивает два массива и если у них одинаковая длина, и значения одинаковы (строгое сравнение) возвращает true. Если нет - false.
 
 
-const t08 = (a, b) => {
-}
+const t08 = (a, b) => [...a].sort().join() === [...b].sort().join();
 
 document.querySelector('.b-8').addEventListener('click', () => console.log(t08([1, 3], [3, 1])));
 
@@ -90,8 +95,7 @@ document.querySelector('.b-8').addEventListener('click', () => console.log(t08([
 //  Напишите функцию, которая проверяет, что в массиве есть минимум одно число 1 и минимум одно число 2. Возвращает true, false.
 
 
-const t09 = (arr) => {
-}
+const t09 = (arr) => arr.includes(1) && arr.includes(2);
 
 document.querySelector('.b-9').addEventListener('click', () => console.log(t09([1, 2, 3, 1, 2, 3])));
 
@@ -99,8 +103,7 @@ document.querySelector('.b-9').addEventListener('click', () => console.log(t09([
 // Task 10
 // Напишите функцию, которая проверяет что в первый массив входят все числа со второго. Возвращает true/false.
 
-const t10 = (a, b) => {
-}
+const t10 = (a, b) => b.every((item) => a.includes(item));
 
 document.querySelector('.b-10').addEventListener('click', () => console.log(t10([1, 2, 3, 1, 2, 3], [1, 2])));
 
